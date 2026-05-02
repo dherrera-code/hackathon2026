4dbc737988a0abfd417f61100204af3ecd7d50bf
dherrera-code <dherr929@gmail.com>
2026-05-02 13:43:07 -0700

    removed top padding on hackathon visualizer!

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans bg-[url('@/public/cristian-palmer-XexawgzYOBc-unsplash.jpg')] bg-fixed min-h-screen">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 m-20 gap-5">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between pb-32 px-16 m-20 gap-5">
 
         <img src="Hackathon-Visualizer-5-2-2026.png" alt="Hackathon visualizer" className='w-[clamp(10rem,50vw,50vw)] m-4 bg-blue-200/80 rounded-3xl flex place-self-center'/>
 

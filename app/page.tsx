94378f8e2ebcc62fdb84357131c14f3388da1af1
dherrera-code <dherr929@gmail.com>
2026-05-02 13:22:17 -0700

    push changes to update title of our website!

--- app/page.tsx
+++ app/page.tsx
@@ -56,8 +56,9 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans bg-[url('@/public/cristian-palmer-XexawgzYOBc-unsplash.jpg')] bg-fixed min-h-screen">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 m-20 gap-5">
-        <img src="Hackathon-Visualizer-5-2-2026.png" alt="Hackathon visualizer" className='w-[clamp(10rem,50vw,50vw)] flex place-self-center'/>
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between pb-32 px-16 m-20 gap-5">
+
+        <img src="Hackathon-Visualizer-5-2-2026.png" alt="Hackathon visualizer" className='w-[clamp(10rem,50vw,50vw)] m-4 bg-blue-200/80 rounded-3xl flex place-self-center'/>
         
         <div className="bg-white rounded-2xl p-4 "><LineChart month={prec.month} chartTitle={prec.chartTitle} chartLabel={prec.chartLabel} dataSets={prec.dataSets}></LineChart></div>
         <div className="bg-white rounded-2xl p-4"><LineChart month={snow.month} chartTitle={snow.chartTitle} chartLabel={snow.chartLabel} dataSets={snow.dataSets}></LineChart></div>

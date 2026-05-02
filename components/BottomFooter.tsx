2d6bf7f1dd7080d771aaf82a0b58ac9674b2787f
dherrera-code <dherr929@gmail.com>
2026-05-02 13:10:13 -0700

    footer from flowbite!

---
+++ components/BottomFooter.tsx
@@ -0,0 +1,20 @@
+import { Footer, FooterCopyright, FooterLink, FooterLinkGroup } from 'flowbite-react'
+import React from 'react'
+
+const BottomFooter = () => {
+    return (
+        <div>
+            <Footer container>
+                <FooterCopyright href="#" by="Sea#" year={2026} />
+                <FooterLinkGroup>
+                    <FooterLink href="#">Daniel Herrera</FooterLink>
+                    <FooterLink href="#">Darcy Rose</FooterLink>
+                    <FooterLink href="#">Isaias Gonzales</FooterLink>
+                </FooterLinkGroup>
+            </Footer>
+
+        </div>
+    )
+}
+
+export default BottomFooter
